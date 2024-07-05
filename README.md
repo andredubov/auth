e6a561aad8c079f9115b73005a0a6df2613be5c0
Andrei Dubov <av.dubov@yandex.ru>
2024-07-05 11:16:10 +0400

    added auth grpc server implementation

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-## Auth microsevice
+## Auth microservice
 
 
 
--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net"
+
+	server "github.com/andredubov/auth/internal/transport/grpc/auth/v1"
+	auth_v1 "github.com/andredubov/auth/pkg/auth/v1"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+)
+
+const grpcPort = 50051
 
 func main() {
-	fmt.Println("Hello world!")
+
+	listen, err := net.Listen("tcp", fmt.Sprintf(":%d", grpcPort))
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	s := grpc.NewServer()
+	reflection.Register(s)
+	auth_v1.RegisterAuthServer(s, server.NewAuthServer())
+
+	log.Printf("server listening at %v", listen.Addr())
+
+	if err = s.Serve(listen); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
 }
