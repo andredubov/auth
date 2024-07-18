bbbe9791cfed9e9c66a5cd8f9b54f7dd11f0b2ad
Andrei Dubov <av.dubov@yandex.ru>
2024-07-18 09:05:15 +0400

    fixed migration files

---
+++ migrations/20240718085501_init_db.sql
@@ -0,0 +1,28 @@
+-- +goose Up
+-- +goose StatementBegin
+CREATE TABLE roles
+(
+    id   serial primary key,
+    name varchar(256) not null unique
+);
+
+INSERT INTO roles (name) VALUES ('user') ON CONFLICT (name) DO NOTHING;
+INSERT INTO roles (name) VALUES ('admin') ON CONFLICT (name) DO NOTHING;
+
+CREATE TABLE users
+(
+    id         serial primary key,
+    name       varchar(256) not null,
+    email      varchar(256) not null unique,
+    pass_hash  varchar,
+    role       int references roles (id) not null on delete cascade,
+    created_at timestamp not null default now(),
+    updated_at timestamp
+);
+-- +goose StatementEnd
+
+-- +goose Down
+-- +goose StatementBegin
+drop table users;
+drop table roles;
+-- +goose StatementEnd
