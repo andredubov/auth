c6b691166846bd8432db2bf8cb0af417b091f734
Andrei Dubov <av.dubov@yandex.ru>
2024-07-20 14:15:40 +0400

    changed migration dns

--- migrator.sh
+++ migrator.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-sleep 2 && goose -dir "${MIGRATION_DIR}" postgres "${MIGRATION_DSN}" up -v
+MIGRATION_DSN="host=${POSTGRES_HOST} port=${POSTGRES_PORT} dbname=${POSTGRES_DB} user=${POSTGRES_USER} password=${POSTGRES_PASSWORD} sslmode=${POSTGRES_SSL_MODE}"
+
+sleep 2 && goose -dir "${MIGRATION_DIR}" postgres "$MIGRATION_DSN" up -v
