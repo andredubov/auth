1b2c9738d6c902599dee6f8c3ca31d651a7a6a1e
Andrei Dubov <av.dubov@yandex.ru>
2024-07-29 18:18:33 +0400

    added update method for the user service layer

---
+++ internal/service/user/update.go
@@ -0,0 +1,20 @@
+package user
+
+import (
+	"context"
+	"log"
+
+	"github.com/andredubov/auth/internal/model"
+)
+
+func (u *usersService) Update(ctx context.Context, updateUserInfo model.UpdateUserInfo) (int64, error) {
+	const op = "usersService.Update:"
+
+	id, err := u.usersRepository.Update(ctx, updateUserInfo)
+	if err != nil {
+		log.Printf("%s: %s", op, err)
+		return 0, err
+	}
+
+	return id, nil
+}
