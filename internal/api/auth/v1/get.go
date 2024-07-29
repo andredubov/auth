2dac8c7d6973ce83fac72864d823f104f15ace6f
Andrei Dubov <av.dubov@yandex.ru>
2024-07-29 18:39:49 +0400

    added get  method for the users api layer

---
+++ internal/api/auth/v1/get.go
@@ -0,0 +1,21 @@
+package server
+
+import (
+	"context"
+
+	"github.com/andredubov/auth/internal/service/converter"
+	auth_v1 "github.com/andredubov/auth/pkg/auth/v1"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+)
+
+func (i *Implementation) Get(ctx context.Context, r *auth_v1.GetRequest) (*auth_v1.GetResponse, error) {
+	user, err := i.usersService.GetByID(ctx, r.GetId())
+	if err != nil {
+		return nil, status.Error(codes.Internal, "failed to get user")
+	}
+
+	return &auth_v1.GetResponse{
+		User: converter.ToUserFromService(user),
+	}, nil
+}
