24b3c7274a409c54b5c4235e932ad0b90eee60a8
Andrei Dubov <av.dubov@yandex.ru>
2024-07-29 18:17:47 +0400

    added create method for the user service layer

---
+++ internal/service/user/create.go
@@ -0,0 +1,29 @@
+package user
+
+import (
+	"context"
+	"log"
+
+	"github.com/andredubov/auth/internal/model"
+)
+
+// Create a new user
+func (u *usersService) Create(ctx context.Context, user model.User) (int64, error) {
+	const op = "usersService.Create:"
+
+	hashedPassword, err := u.hasher.HashAndSalt(user.Password)
+	if err != nil {
+		log.Printf("%s: %s", op, err)
+		return 0, err
+	}
+
+	user.Password = hashedPassword
+
+	id, err := u.usersRepository.Create(ctx, user)
+	if err != nil {
+		log.Printf("%s: %s", op, err)
+		return 0, err
+	}
+
+	return id, err
+}
