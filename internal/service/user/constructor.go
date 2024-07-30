bfbe31716571a5732d1eb704f4521627e69ba432
Andrei Dubov <av.dubov@yandex.ru>
2024-07-30 17:14:06 +0400

    fixed user service layer

--- internal/service/user/constructor.go
+++ internal/service/user/constructor.go
@@ -1,6 +1,7 @@
 package user
 
 import (
+	"github.com/andredubov/auth/internal/client/database"
 	"github.com/andredubov/auth/internal/repository"
 	"github.com/andredubov/auth/internal/service"
 	"github.com/andredubov/auth/pkg/hasher"
@@ -9,11 +10,17 @@ import (
 type usersService struct {
 	usersRepository repository.Users
 	hasher          hasher.PasswordHasher
+	txManager       database.TxManager
 }
 
-func NewService(usersRepository repository.Users, hasher hasher.PasswordHasher) service.Users {
+func NewService(
+	usersRepository repository.Users,
+	hasher hasher.PasswordHasher,
+	txManager database.TxManager,
+) service.Users {
 	return &usersService{
 		usersRepository,
 		hasher,
+		txManager,
 	}
 }
