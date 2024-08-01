7266d2811726d8b1b7ec2ecaaca6292ec820a00d
Andrei Dubov <av.dubov@yandex.ru>
2024-08-01 19:48:26 +0400

    fixed api layer

--- internal/api/auth/v1/constructor.go
+++ internal/api/auth/v1/constructor.go
@@ -5,11 +5,13 @@ import (
 	auth_v1 "github.com/andredubov/auth/pkg/auth/v1"
 )
 
+// Implementation ...
 type Implementation struct {
 	auth_v1.UnimplementedAuthServer
 	usersService service.Users
 }
 
+// NewImplementation creates an instance of Implementation struct
 func NewImplementation(service service.Users) *Implementation {
 	return &Implementation{
 		usersService: service,
