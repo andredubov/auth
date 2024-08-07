2c6f5dab4c1e2210d35f759ad2ae9004c257e462
Andrei Dubov <av.dubov@yandex.ru>
2024-08-07 15:36:44 +0400

    moved out some golang packages

--- pkg/hasher/hasher.go
+++
@@ -1,7 +0,0 @@
-package hasher
-
-// PasswordHasher provides hashing logic to securely store passwords.
-type PasswordHasher interface {
-	HashAndSalt(plainPassword string) (string, error)
-	ComparePasswords(hashedPassword string, plainPassword string) error
-}
